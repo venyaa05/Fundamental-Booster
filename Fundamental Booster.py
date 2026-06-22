8a9eb373f40d13c1b61223bc210fcfc11005a148
venyaa05 <dholariyavenya@gmail.com>
2026-06-22 11:35:13 +0530

    Fundamental Booster.py

--- Fundamental Booster.py
+++ Fundamental Booster.py
@@ -9,11 +9,12 @@ print("Thank you! Here is the information we collected:")
 print()
 print("Name:",name,"(Type:",type(name),"Memory Address:",id(name),")")
 print("Age:",age,"(Type:",type(age),"Memory Address:",id(age),")")
-print("Height:",name,"(Type:",type(height),"Memory Address:",id(height),")")
-print("Favorite_number:",name,"(Type:",type(favourite_number),"Memory Address:",id(favourite_number),")")
+print("Height:",height,"(Type:",type(height),"Memory Address:",id(height),")")
+print("Favorite_number:",favourite_number,"(Type:",type(favourite_number),"Memory Address:",id(favourite_number),")")
 print()
 current_year=2026
 birth_year=current_year - age
 print("Your birth year is approximately:",birth_year,"based on your age of:",age)
 print()
 print("Thank you for using the Personal Data Collector.Goodbye")
+
